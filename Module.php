cdfaaabaa9a961e2f64310eb323b3401b44ff3a2
soovorow <soovorow@gmail.com>
2017-04-10 18:20:07 +0300

    init

---
+++ Module.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace soovorow\letter_ape;
+
+/**
+ * Letter ape module
+ *
+ * Add following code into your `config\main.php` for start using it
+ *
+ * 'modules' => [
+ *     'letter_ape' => [
+ *         'class' => 'soovorow\letter_ape\Module',
+ *     ],
+ * ],
+ *
+ * @author Dmitry Suvorov <soovorow@gmail.com>
+ *
+ */
+class Module extends \yii\base\Module
+{
+    public function init()
+    {
+        parent::init();
+    }
+}
